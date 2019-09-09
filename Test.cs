0cf4d67c4e87cc957d99ad24177a59cbb71bb10c
milkadiazcleto <31774681+milkadiazcleto@users.noreply.github.com>
2019-09-08 22:19:13 -0400

    7792 : Ticket sobre empresa tal

--- Test.cs
+++ Test.cs
@@ -3,7 +3,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-            Console.WriteLine("dsHffdsffffolccedsfdsdimgopfesojsdsfssfffdsdsfdfdsfdsfhjdssnjnjmnsarrrsaars");
+            Console.WriteLine("dsHffdsfdfdsffffolccedsfdsdimgopfesojsdsfssfffdsdsfdfdsfdsfhjdssnjnjmnsarrrsaars");
         }
          
     }
